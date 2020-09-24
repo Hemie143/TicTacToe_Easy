c5c66400595a15efd000922c61ae087ace317f21
hemie143 <hemie143@gmail.com>
2020-09-24 22:38:34 +0200

    Stage 2

--- Tic-Tac-Toe/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe/task/tictactoe/tictactoe.py
@@ -1,1 +1,10 @@
-print('X O O\n X X O \nO X X')
+
+print('Enter cells: ')
+grid_input = input()
+# print('X O O\n X X O \nO X X')
+
+print('--------')
+print(f'| {" ".join(grid_input[:3])  } |')
+print(f'| {" ".join(grid_input[3:6])  } |')
+print(f'| {" ".join(grid_input[6:])  } |')
+print('---------')
